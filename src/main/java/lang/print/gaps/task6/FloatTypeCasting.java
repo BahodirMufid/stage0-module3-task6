043c3163bbdeb557b2bb850617c117e241d6bd2b
bahod <bahodirmufid@gmail.com>
2022-09-29 12:04:58 +0500

    all files up to date

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+
+
+    public static void main(String[] args) {
+        roundNumber(12.9f);
+    }
+
+    public static void roundNumber(float numberToBeRounded) {
+
+        System.out.println(((int) numberToBeRounded));
+
     }
 }
